6dfadd00fb12985dcd62daee476fbd1160b7a0e9
namrathasubramanya <namratha.subramanya@gmail.com>
2019-04-20 16:17:06 -0700

    gitignore updated

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:admin@user-data-joxqv.mongodb.net/users?retryWrites=true",
-  secretOrKey: "secret",
-  visionAPI: "AIzaSyD3OvnQIWVnRuT3_xD-hto1xk4kn74G3xs",
-  visionAuth:
-    "Bearer ya29.c.El_yBlDu-e-30P0lHj6MUPd99zxYYPOjO54Siim0kaV-PchSZ2q3A_iekpouAtzMv6uiho0CbD58__ZRC6ozoI1I11YDAvsViJv2I4Rdxoea-dmyUAwX6KE_Q1lzPsaF0Q"
-};
